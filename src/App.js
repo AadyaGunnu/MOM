ef2ac98d48831e71696ac9d5d48c9cc9b878511f
AadyaGunnu <102150537+AadyaGunnu@users.noreply.github.com>
2022-03-22 18:20:13 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Favourites  from './components/Favourites';
+import Search  from './components/Search';
+
+function App() {
+  return (
+    <div >
+      <span className='App-header'>Welcom to Mad Over Movies</span>
+      <div className='App-nav'>
+      <Search></Search>
+       
+      </div>
+       </div>
+  );
+}
+
+export default App;
